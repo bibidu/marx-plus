36d2681ba3e7d50da94882305d5893a5188d834e
duxianzhang <duxianzhang@meituan.com>
2022-01-19 23:28:57 +0800

    feat

--- marx-export-button/src/index.ts
+++ marx-export-button/src/index.ts
@@ -2,7 +2,7 @@ const icon = "https://api.iconify.design/bytesize:export.svg";
 
 export function activate(context) {
   console.log('[marx-export-button] activate!')
-  const exportButton = context.topbar.toolbar.register({
+  const exportButton = marx.topbar.toolbar.register({
     icon,
   });
 }
