87de084fe4746b6a175340d2c81e6432f83e7cc3
duxianzhang <duxianzhang@meituan.com>
2022-01-21 23:30:57 +0800

    feat

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,20 +1,30 @@
 import { defineConfig, Options } from "tsup";
 import { replace } from "esbuild-plugin-replace";
 
-export default defineConfig({
-  entry: [
-    "packages/marx-export-button/src/index.ts",
-    "packages/marx-inject-template-button/src/index.ts",
-  ],
-  outDir: 'dist',
-  clean: true,
-  format: ["cjs"],
-  // format: ["esm", "cjs", "iife"],
-  legacyOutput: true,
-  noExternal: ["html2canvas"],
-  esbuildPlugins: [
-    replace({
-      __DEV__: JSON.stringify(process.env.NODE_ENV),
-    }),
-  ],
-});
+export default defineConfig([
+  {
+    entry: ["packages/marx-export-button/src/index.ts"],
+    outDir: "dist/marx-export-button",
+    clean: true,
+    format: ["cjs"],
+    legacyOutput: true,
+    noExternal: ["html2canvas"],
+    esbuildPlugins: [
+      replace({
+        __DEV__: JSON.stringify(process.env.NODE_ENV),
+      }),
+    ],
+  },
+  {
+    entry: ["packages/marx-inject-template-button/src/index.ts"],
+    outDir: "dist/marx-inject-template-button",
+    clean: true,
+    format: ["cjs"],
+    legacyOutput: true,
+    esbuildPlugins: [
+      replace({
+        __DEV__: JSON.stringify(process.env.NODE_ENV),
+      }),
+    ],
+  },
+]);
