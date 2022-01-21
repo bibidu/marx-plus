98bc47b6389fd3703a7df48f3e20ddb230a5cf31
duxianzhang <duxianzhang@meituan.com>
2022-01-21 23:10:36 +0800

    feat

--- scripts/build.js
+++
@@ -1,5 +0,0 @@
-const { execSync } = require('child_process')
-
-execSync(`
-pnpm run build --filter @marx/inject-template-button --filter @marx/export-button
-`)
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,18 +1,20 @@
-import type { Options } from "tsup";
+import { defineConfig, Options } from "tsup";
 import { replace } from "esbuild-plugin-replace";
 
-export function defineConfig(options?: Options): Options {
-  return {
-    clean: true,
-    format: ["cjs"],
-    // format: ["esm", "cjs", "iife"],
-    legacyOutput: true,
-    noExternal: ["html2canvas"],
-    esbuildPlugins: [
-      replace({
-        __DEV__: JSON.stringify(process.env.NODE_ENV),
-      }),
-    ],
-    ...(options || {}),
-  };
-}
+export default defineConfig({
+  entry: [
+    "packages/marx-export-button/src/index.ts",
+    "packages/marx-inject-template-button/src/index.ts",
+  ],
+  outDir: 'dist',
+  clean: true,
+  format: ["cjs"],
+  // format: ["esm", "cjs", "iife"],
+  legacyOutput: true,
+  noExternal: ["html2canvas"],
+  esbuildPlugins: [
+    replace({
+      __DEV__: JSON.stringify(process.env.NODE_ENV),
+    }),
+  ],
+});
